a0a8ef1f3e6ba2f40d1a4470d5764ebf6c0f7764
MarcusGrass <marcus.grass@gmail.com>
2021-03-22 07:43:39 +0100

    Xmonad update on bt conf minor improvement

--- bash/bt_clear.sh
+++ bash/bt_clear.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bluetoothctl -- untrust 4C:87:5D:2C:57:6A
 bluetoothctl -- remove 4C:87:5D:2C:57:6A
-bluetoothctl -- scan on
+timeout 5s bluetoothctl -- scan on
 bluetoothctl -- trust 4C:87:5D:2C:57:6A
 bluetoothctl -- pair 4C:87:5D:2C:57:6A
 bluetoothctl -- connect 4C:87:5D:2C:57:6A
